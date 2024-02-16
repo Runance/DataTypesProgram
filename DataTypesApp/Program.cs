0f2f7e81169a900455db07d4181c65e75d080a45
Aguilar.304743 <Aguilar.304743@LAB-B-PC34>
2024-02-16 09:15:26 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DataTypesApp
+{
+    internal class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the pieces of apple: ");
+            int Apples = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the price of " + Apples + " apple(s): ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of " + Apples + " apples(s) is " + price );
+
+            int c_price = (int)price;
+            Console.WriteLine("The value of original price is " + price);
+            Console.WriteLine("The value of converted price is " + c_price);
+
+
+
+        }
+    }
+}
